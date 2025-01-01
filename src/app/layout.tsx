90ea61b469b3de711e243bdc53f6950b008bd124
sarthaklaptop <sarthaklaptop402@gmail.com>
2025-01-01 19:47:49 +0530

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "SoarUI - Tailwind Component Library",
+  description: "SoarUI is a collection of beautiful, ready-to-use UI components built with Tailwind CSS and Next.js. Copy, paste, and build stunning applications effortlessly—no installation required.",
+};
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
